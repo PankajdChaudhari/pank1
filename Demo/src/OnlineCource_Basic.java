e3bef7a2353760b989c8fb673807c852913ea980
Mahavir <Mahavir@Pankaj-PC>
2022-06-02 13:42:33 +0530

    hi

---
+++ Demo/src/OnlineCource_Basic.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OnlineCource_Basic {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
